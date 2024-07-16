e5a2608141e31c131bf76e16d50ea41618ae8d5e
Mauri <maur.gatica@duocuc.cl>
2024-07-15 21:20:05 -0400

    avances

--- M2A/Store/views.py
+++ M2A/Store/views.py
@@ -221,19 +221,13 @@ def subirJuego(request):
         idJuego       = request.POST['txtId']
         nombre        = request.POST['nombre']
         descripcion   = request.POST['descripcion']
-<<<<<<< HEAD
         ytVidId       = 0
-=======
-        ytVidId       = request.POST['link']
->>>>>>> a8a32ed78e32779448f3105f0b903407733eba4f
         precio        = request.POST['precio']
         stock         = request.POST['stock']
 
         # buscar youtube id
         #m =  re.search(r"([\d\w-_]{11})", ytVidId)
         #ytVidId = m.group()
-        
-        #convertir idjuego en un string para comparar
         str(idJuego)
         if 'enviarJuego' in request.POST:
             if idJuego == "0":
@@ -243,11 +237,7 @@ def subirJuego(request):
                     categoria = categoriaJuego.objects.get(idCategoria=request.POST['categoriaJuego']),
                     descripcion = descripcion,
                     imagen = request.FILES['imagen'],
-<<<<<<< HEAD
                     ytVidId = None,
-=======
-                    ytVidId = ytVidId,
->>>>>>> a8a32ed78e32779448f3105f0b903407733eba4f
                     precio = precio,
                     stock = stock,
                     clave = request.FILES['archivo'],
