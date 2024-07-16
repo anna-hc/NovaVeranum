f1058512b24bde88ce8fd9435336190b9fdfa36e
AnnaHeisig <an.heisig@duocuc.cl>
2024-07-16 16:22:29 -0400

    reservas

--- M2A/Store/admin.py
+++ M2A/Store/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from .models import Habitacion, Serie, imagenSerie, categoriaSerie, imgHab, tipoHab, nivelEducacional, Usuario, Region, dispHab, servicio
+from .models import Habitacion, imgHab, tipoHab, nivelEducacional, Usuario, Region, dispHab, servicio, Reserva
 
 # Register your models here.
 admin.site.register(Habitacion)
-admin.site.register(Serie)
+admin.site.register(Reserva)
 admin.site.register(Usuario)
-admin.site.register(imagenSerie)
 admin.site.register(Region)
 admin.site.register(imgHab)
-admin.site.register(categoriaSerie)
 admin.site.register(tipoHab)
 admin.site.register(nivelEducacional)
 admin.site.register(servicio)
