53b4c088138b4ba2ba5078c3dabe56fb75f1c928
AnnaHeisig <an.heisig@duocuc.cl>
2024-07-16 18:41:08 -0400

    style

--- M2A/Store/views.py
+++ M2A/Store/views.py
@@ -183,8 +183,7 @@ def eliminarHabCarro(request, idHab):
     try:
         carritoSesion = request.session.get('carrito', {})
         if str(idHab) in carritoSesion:
-            if carritoSesion[str(idHab)]['cantidad'] >= 1:
-                del carritoSesion[str(idHab)]
+            del carritoSesion[str(idHab)]
         request.session['carrito'] = carritoSesion
         return redirect(verCarro)
         #usuario = request.user
